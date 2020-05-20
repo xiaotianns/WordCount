9b652419c33b7245689db3b7c5d3cc0097fc669d
xiaotianns <1271510083@qq.com>
2020-05-21 00:02:30 +0800

    xiaotianns

--- WordCount.cpp
+++ WordCount.cpp
@@ -1,146 +1,74 @@
 #include <stdio.h>
-#include <string.h>
-#include<malloc.h>
-int countw(char *filename);
-int countc(char *filename);
-int countl(char *filename);
+#include <stdlib.h>
 
-int main(int argc, char* argv[]){
-	char filname[30];
-	char operation;
-	int totalline;//???
-	int toalchar;//????
-	int totalword;//????
-	if (!strcmp(argv[1], "-w"))
-		countw(argv[2]);
-	else if (!strcmp(argv[1], "-c"))
-
-		countc(argv[2]);
-	else if (!strcmp(argv[1], "-l"))
-		countl(argv[2]);
-	return 0;
+int main()
+{
+ int charcount(char *filename);
+ int wordcount(char *filename);
+ FILE *fp;
+ int c_count,w_count;
+ char ch[3],filename[100];
+ printf("输入用户命令.exe ");
+ scanf("%s %s",ch,filename);
+ if((fp=fopen(filename,"r"))==NULL)
+ {
+     printf("filenull.\n");
+     exit(-1);
+ }
+ //获取命令和输出
+ if(ch[1]=='c')
+ {
+    c_count=charcount(filename);
+    printf("字符数=%d\n",c_count);
+ }
+ if(ch[1]=='w')
+ {
+    w_count=wordcount(filename);
+    printf("单词数=%d\n",w_count);
+ }
+    return 0;
 }
-
-int countw(char *filename){
-	FILE *fp = NULL;
-	FILE *fp2 = NULL;
-	char buffer[1003];
-	int bufferLen;
-	int i;
-	char c;
-	int isLastBlank = 0;
-	int totalword = 0;
-	int wordNum = 0;
-	if ((fp = fopen(filename, "rb")) == NULL){
-		perror(filename);
-		return NULL;
-	}
-
-	while (fgets(buffer, 1003, fp) != NULL){
-		bufferLen = strlen(buffer);
-
-		for (i = 0; i<bufferLen; i++){
-			c = buffer[i];
-			if (c == ' ' || c == '\t'){
-				!isLastBlank && wordNum++; //????????,???????????+1 
-				isLastBlank = 1;//??????
-			}
-			else if (c != '\n'&&c != '\r'){
-
-				isLastBlank = 0;
-			}
-		}
-		!isLastBlank && wordNum++;
-		isLastBlank = 1;
-		totalword += wordNum;
-		wordNum = 0;
-	}
-	printf("totalword:%d ", totalword);
-	fp2 = fopen("result.txt", "a");
-	if (fp2){
-		fprintf(fp2, "????:%d\n", totalword);
-		fclose(fp2);
-	}
-	return 0;
+int charcount(char *filename)//统计字符数
+{
+    int n1=0;
+    char ch;
+    FILE *fp;
+    if((fp=fopen(filename,"r"))==NULL)
+    {
+        exit(-1);
+    }
+    ch=fgetc(fp);
+    while(!feof(fp))
+    {
+        ch=fgetc(fp);
+        n1++;
+    }
+    fclose(fp);
+    return n1;
 }
-
-
-int countc(char *filename){
-
-	FILE *fp = NULL;
-	FILE *fp2 = NULL;
-	char buffer[1003];
-	int bufferLen;
-	int i;
-	char c;
-	int isLastBlank = 0;
-	int totalchar = 0;
-	int charNum = 0;
-	if ((fp = fopen(filename, "rb")) == NULL){
-		perror(filename);
-		return NULL;
-	}
-	while (fgets(buffer, 1003, fp) != NULL){
-		bufferLen = strlen(buffer);
-		for (i = 0; i<bufferLen; i++){
-			c = buffer[i];
-			if (c == ' ' || c == '\t'){
-				isLastBlank = 1;//???????
-			}
-			else if (c != '\n'&&c != '\r'){
-				charNum++;
-				isLastBlank = 0;
-			}
-		}
-
-		isLastBlank = 1;
-
-		totalchar += charNum;
-
-		charNum = 0;
-
-	}
-	printf("totalchar:%d", totalchar);
-	fp2 = fopen("result.txt", "a");
-	if (fp2){
-		fprintf(fp2, "????:%d\n", totalchar);
-		fclose(fp2);
-	}
-	return 0;
+int wordcount(char *filename)//统计单词数
+{
+    int n2=0,word=0;
+    char ch;
+    FILE *fp;
+    if((fp=fopen(filename,"r"))==NULL)
+    {
+        exit(-1);
+    }
+    ch=fgetc(fp);
+    while(!feof(fp))
+    {
+        ch=fgetc(fp);
+        if(ch<'A'||(ch>'Z'&&ch<'a')||ch>'z')
+            word=0;
+        else if(word==0)
+        {
+            word=1;
+            n2++;
+        }
+    }
+    fclose(fp);
+    return n2;
 }
-
-int countl(char *filename){
-	FILE *fp = NULL;
-	FILE *fp2 = NULL;
-	char buffer[1003];
-	int bufferLen;
-	int i;
-	char c;
-	int totalline = -1;
-	if ((fp = fopen(filename, "rb")) == NULL){
-		perror(filename);
-		return NULL;
-	}
-
-	while (fgets(buffer, 1003, fp) != NULL){
-		bufferLen = strlen(buffer);
-		for (i = 0; i<bufferLen; i++){
-			c = buffer[i];
-			if (c == '\n' || c == '\r'){
-				totalline++;
-
-			}
-		}
-	}
-	printf("totalline:%d", totalline);
-	fp2 = fopen("result.txt", "a");
-	if (fp2){
-		fprintf(fp2, "???:%d\n", totalline);
-		fclose(fp2);
-	}
-	return 0;
-
-}
-
 
 
